1a5fb3e11abfd0faea3e9cd5b301b0f319ef9bfb
tslight <tobyslight@gmail.com>
2019-03-18 21:13:02 +0000

    can't split this up as multiple returns... needs a refactor

--- chopt/chopt.py
+++ chopt/chopt.py
@@ -9,30 +9,6 @@ from .matches import get_matches
 from .mark import mark, chkmrk
 
 
-def choose(markall, inputs, options, chosen):
-    if re.match('^t(oggle)?$', inputs[0], re.IGNORECASE):
-        if markall:
-            for o in options:
-                chosen[options.index(o)] = "   "
-        else:
-            for o in options:
-                chosen[options.index(o)] = " + "
-    elif re.match('^r(eset)?$', inputs[0], re.IGNORECASE):
-        for o in options:
-            chosen[options.index(o)] = "   "
-    elif re.match('^a(ccept)?$', inputs[0], re.IGNORECASE):
-        # list comprehension that returns all chosen options
-        return [o for o in options if chosen[options.index(o)] == " + "]
-    elif re.match('^q(uit)?$', inputs[0], re.IGNORECASE):
-        return
-    else:
-        inputs = get_ranges(inputs, options)
-        matched, invalid, failed = get_matches(inputs, options)
-        chosen = mark(matched, chosen)
-
-    return chosen
-
-
 def chopt(options):
     '''
     Takes a list of options as an argument and returns a list of selected
@@ -45,16 +21,41 @@ def chopt(options):
 
     while True:
         failed = False
+
         menu(options, chosen)
+
         if output:
             print(output)
+
         # get list of inputs split on spaces
         inputs = input("\n----> ").split(" ")
-        chosen = choose(markall, inputs, options, chosen)
+
+        if re.match('^t(oggle)?$', inputs[0], re.IGNORECASE):
+            if markall:
+                for o in options:
+                    chosen[options.index(o)] = "   "
+            else:
+                for o in options:
+                    chosen[options.index(o)] = " + "
+        elif re.match('^r(eset)?$', inputs[0], re.IGNORECASE):
+            for o in options:
+                chosen[options.index(o)] = "   "
+        elif re.match('^a(ccept)?$', inputs[0], re.IGNORECASE):
+            # list comprehension that returns all chosen options
+            return [o for o in options if chosen[options.index(o)] == " + "]
+        elif re.match('^q(uit)?$', inputs[0], re.IGNORECASE):
+            return
+        else:
+            inputs = get_ranges(inputs, options)
+            matched, invalid, failed = get_matches(inputs, options)
+            chosen = mark(matched, chosen)
+
         markall = chkmrk(options, chosen)
+
         if failed:
             fmt = "\nINVALID INPUTS: {}"
             msg = ", ".join(invalid)
         else:
             fmt, msg = ("",)*2
+
         output = fmt.format(msg)
